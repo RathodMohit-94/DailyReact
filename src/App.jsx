a6656fa6ebe87bfbda910d3d74fa2e159282243c
RathodMohit-94 <mohitrathod9328@gmail.com>
2025-06-12 15:09:19 +0530

    Changes In main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState , useEffect } from "react"
 import State from "./components/State"
+import Effect from "./components/Effect"
+import Ref from "./components/Ref"
+
 
 function App() {
+  
+  
+  useEffect(() => {  
+      alert("Welcome to Website. If load twice turn off srtictmode")
+  }, [])
+  
+
+
+
+
   return (
     <>
-      <State />
+      <State /> {/* For  UseState.. */}
+      <Effect /> {/* For  UseEffect.. */}
+      <Ref />
     </>
   )
 }
