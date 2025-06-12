33677d1c2fc0f9c3dc5b3686960113a2646ea97b
RathodMohit-94 <mohitrathod9328@gmail.com>
2025-06-12 15:09:51 +0530

    UseState Added

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
-import './State.css'
+import React, { useState, useEffect } from "react";
 
 const State = () => {
-  const [ count , setCount] = useState(0);
-    return (
-    <div className='State'>
-        <h1 className='Stateh1'>USE STATE </h1>
-        <div className='State1'>
-            <p>The Count Is {count} </p>
-            <button onClick={()=>{setCount(count+1)}}>Click Me</button>
-            <button onClick={()=>{setCount(0)}}>Reset</button>
-        </div>
-    </div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default State
+  // const a = count;
+
+  // useEffect(() => {
+  //     alert("Clicked " +a+ " Times,HOOORRRAAAYYY...");
+  // }, [count]);
+
+  
+  return (
+    <div className="State">
+      <h1 className="Stateh1">USE STATE </h1>
+      <div className="State1">
+        <p>The Count Is {count} </p>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Click Me
+        </button>
+        <button
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default State;
